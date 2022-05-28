a794c47b9fef8378698b4fc745ca119ddf3b7a77
Mohammad Al-Ghafli <m7md2012g@gmail.com>
2022-05-28 22:46:20 +0300

    user creation test

--- auth/src/Errors/RouteError.ts
+++ auth/src/Errors/RouteError.ts
@@ -1,6 +1,7 @@
 export class RouteError extends Error {
     // an error of this class will be thrown if any error catched on the routes
     constructor(message: string, public statusCode: number) {
+        console.log(message);
         super(message);
         Object.setPrototypeOf(this, RouteError.prototype);
     }
--- auth/src/controllers/auth.controller.ts
+++ auth/src/controllers/auth.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express';
+import { RouteError } from '../Errors/RouteError';
+import { User } from '../models/user.model'; // the user model
 
 export const getUser = (req: Request, res: Response) => {
     res.status(200).send('Hi there!');
 }
 
-export const signUp = (req: Request, res: Response) => {
+export const signUp = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    res.status(200).send({ email, password });
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        throw new RouteError('Email in use', 400);
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    console.log('the user is created');
+    res.status(201).send(user); // 201 means the record is created successfully!
+    
 }
