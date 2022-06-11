776aff6c50f8af0742d60fd4fca701c5e78c1005
Mohammad Al-Ghafli <m7md2012g@gmail.com>
2022-06-11 16:32:37 +0300

    current user middleware

--- auth/src/routes/auth.route.ts
+++ auth/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express';
 import { getUser, signIn, signOut, signUp } from '../controllers/auth.controller';
+import { currentUser } from '../middleware/authMiddleware';
 import { emailRequired, passwordRequired, showMessages } from '../middleware/bodyCheckers';
 
 export const authRoutes = (app: Express) => {
     // here we will list all the routes for the authentication
-    app.get('/api/auth/user', getUser);
+    app.get('/api/auth/user', currentUser, getUser);
 
     app.post('/api/auth/signout', signOut);
 
