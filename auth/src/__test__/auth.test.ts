7895f7103b185bfdf990c2ac477f00cc26e6b320
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2023-08-13 22:56:38 +0300

    signup route first test

---
+++ auth/src/__test__/auth.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest";
+import { app } from "../app";
+
+it("should returns 201 after successful signup request", async () => {
+  await request(app)
+    .post("/api/auth/signup")
+    .send({
+      email: "test@test.com",
+      password: "Test1234",
+    })
+    .expect(201);
+});
+
+it("should returns a status code 400, after sending an invalid email", async () => {
+  await request(app)
+    .post("/api/auth/signup")
+    .send({
+      email: "incorrect email",
+      password: "Test1234",
+    })
+    .expect(400);
+});
+
+it("should returns a status code 400, if we have less than 8 characters password length", async () => {
+  await request(app)
+    .post("/api/auth/signup")
+    .send({
+      email: "test@test.com",
+      password: "wrong",
+    })
+    .expect(400);
+});
+
+it("should returns a status 400, if the body is missing the email or password", async () => {
+  // test if we sent email only
+  await request(app)
+    .post("/api/auth/signup")
+    .send({ email: "test@test.com" })
+    .expect(400);
+
+  // test if we sent password only
+  await request(app)
+    .post("/api/auth/signup")
+    .send({ password: "Test1234" })
+    .expect(400);
+});
--- auth/src/config/setupTest.ts
+++ auth/src/config/setupTest.ts
@@ -1,15 +1,23 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import { app } from "../app";
 
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
+  // setup the environment variables
+  process.env.JWT_KEY = "asdfasdf";
+
+  // create a mongodb memory server instance
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
+  // establish the connection between mongoose and the server
+  mongoose.set("strictQuery", false);
   await mongoose.connect(mongoUri, {});
 });
 
+// before starting any test reset the database
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
 
@@ -18,6 +26,7 @@ beforeEach(async () => {
   }
 });
 
+// after the test clean up the db
 afterAll(async () => {
   await mongo?.stop();
   await mongoose.connection.close();
