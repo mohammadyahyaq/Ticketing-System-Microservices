647d0acac7a7308b134a757371cfc3e0de68e90e
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-02-26 23:47:26 +0300

    adding type safety + refactoring

---
+++ nats-test/src/events/TicketCreatedListener.ts
@@ -0,0 +1,14 @@
+import { Message } from "node-nats-streaming";
+import { Listener } from "./baseListener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject = Subjects.TicketCreated;
+  queueGroupName = "paymentsService";
+
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    console.log("Event data", data);
+    msg.ack();
+  }
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/TicketCreatedListener";
 
 // we want to make a random client ID for each service to allow making replicas of this service
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -15,32 +16,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  // we want to make the nats service to wait for us to send acknowledgement when we processed the message correctly
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    // if service got started we want to get all previous events
-    .setDeliverAllAvailable()
-    // when service is down for a while we want to get all missed events
-    .setDurableName("ticket-service");
-
-  // the second argument allows us to make a replica of this service
-  // were this service will receive only one message to this queue group
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "listenerQueueGroup",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}`);
-      console.log("data:", data);
-    }
-    // send the acknowledgement to NATS service
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 // close connection on interrupt
