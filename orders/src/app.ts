c5b7f6c31883d439449194a92366a41e8e8387b5
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-04-20 17:25:10 +0300

    setting up the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+
+// this library will allow express to use error handlers in async api's
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { currentUser, errorHandler } from "@mohammadyahyaq-learning/common";
+// import { ticketsRoutes } from "./routes/tickets.route";
+
+const app = express();
+
+// setup the initial middlewares
+app.set("trust proxy", true);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false, // to remove encryption
+    // restrict the cookie to only work in https connection in non test environment
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+// ticketsRoutes(app);
+
+// now we add the error handler
+app.use(errorHandler);
+
+export { app };
