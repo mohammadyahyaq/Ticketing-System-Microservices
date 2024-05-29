a510645638fcf6a0f3892baef5471585d65f5a02
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-05-29 23:27:35 +0300

    wire the listeners to the singleton nats client

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,6 +4,8 @@ import "./config/database";
 import { app } from "./app";
 
 import { singletonNatsClient } from "./config/SingletonNatsClient";
+import { OrderCreatedListener } from "./listeners/OrderCreatedListener";
+import { OrderCancelledListener } from "./listeners/OrderCancelledListener";
 
 (async () => {
   if (!process.env.NATS_CLUSTER_ID) {
@@ -30,6 +32,9 @@ import { singletonNatsClient } from "./config/SingletonNatsClient";
     process.on("SIGINT", () => singletonNatsClient.client.close());
     // close connection on terminate
     process.on("SIGTERM", () => singletonNatsClient.client.close());
+
+    new OrderCreatedListener(singletonNatsClient.client).listen();
+    new OrderCancelledListener(singletonNatsClient.client).listen();
   } catch (error) {
     console.log(error);
   }
