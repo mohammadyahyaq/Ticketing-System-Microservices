77ac709c60d7ffc7c0b782026fee0277e5f7ade4
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-06-01 18:44:57 +0300

    adding orders model in payments service

--- payments/src/models/order.model.ts
+++ payments/src/models/order.model.ts
@@ -1,14 +1,13 @@
 import { OrderStatus } from "@mohammadyahyaq-learning/common";
 import { Document, Model, Schema, model } from "mongoose";
-import { TicketDoc } from "./ticket.model";
-import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 // interface that represents that type of Ticket.build() parameter
 interface OrderAttrs {
-  userId: string;
+  id: string;
   status: OrderStatus;
-  expiresAt: Date;
-  ticket: TicketDoc;
+  userId: string;
+  price: number;
+  version: number;
 }
 
 // interface that represents the Ticket model where we will add all the statics methods
@@ -18,38 +17,30 @@ interface OrderModel extends Model<OrderDoc> {
 
 // interface that represents the Ticket document (after the Ticket created)
 interface OrderDoc extends Document {
-  userId: string;
   status: OrderStatus;
-  expiresAt: Date;
-  ticket: TicketDoc;
+  userId: string;
+  price: number;
   version: number;
-  createdAt: string;
-  updatedAt: string;
 }
 
 const orderSchema = new Schema(
   {
-    userId: {
-      type: String,
-      required: true,
-    },
     status: {
       type: String,
       required: true,
       enum: Object.values(OrderStatus),
       default: OrderStatus.Created,
     },
-    expiresAt: {
-      type: Schema.Types.Date,
+    userId: {
+      type: String,
+      required: true,
     },
-    ticket: {
-      type: Schema.Types.ObjectId,
-      ref: "Ticket",
+    price: {
+      type: Number,
       required: true,
     },
   },
   {
-    timestamps: true,
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
@@ -58,9 +49,6 @@ const orderSchema = new Schema(
     },
   }
 );
-
-orderSchema.set("versionKey", "version");
-orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
