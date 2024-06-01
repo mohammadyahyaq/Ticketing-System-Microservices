6829dde9a4ad7106db43182c6bcce08b38777151
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-06-01 16:34:09 +0300

    listen to expiration complete event in orders service

---
+++ orders/src/listeners/ExpirationCompleteListener.ts
@@ -0,0 +1,36 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+} from "@mohammadyahyaq-learning/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import { Order } from "../models/order.model";
+import { OrderCancelledPublisher } from "../publishers/OrderCancelledPublisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
