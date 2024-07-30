9cd01f12a2724c4b13b542ef97eae0bbc86f1d52
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-07-30 21:39:33 +0300

    test pipeline test failed

--- auth/src/config/database.ts
+++ auth/src/config/database.ts
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 const start = async () => {
-  console.log("Starting up 3");
   if (!process.env.JWT_KEY) {
     throw Error("JWT_KEY is not defined");
   }
