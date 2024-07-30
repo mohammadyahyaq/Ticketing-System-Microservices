2d137225cd967abadb5ee6e1f3568153a01b160a
Mohammed Alghafli <mohammedyahya@Mohammeds-MacBook-Pro-2.local>
2024-07-30 22:08:15 +0300

    some changes

--- auth/src/config/database.ts
+++ auth/src/config/database.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 const start = async () => {
+  console.log("Starting testing ci");
   if (!process.env.JWT_KEY) {
     throw Error("JWT_KEY is not defined");
   }
